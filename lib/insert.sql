796d57e01d08fca885aa3188c0f6e0df3010ee23
jessicashannon <jessdahling@gmail.com>
2015-08-14 14:26:06 -0400

    done

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,64 @@
--- your code here
+-- CREATE TABLE projects (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   title TEXT,
+--   category TEXT,
+--   funding_goal INTEGER,
+--   start_date DATE,
+--   end_date DATE
+-- )
+
+INSERT INTO projects (title, category, 
+  funding_goal, start_date, end_date) 
+VALUES ('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015),
+('a title', 'a category', 450, 07-30-1985, 09-12-2015);
+
+-- CREATE TABLE users (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER
+--   );
+
+INSERT INTO users (name, age) 
+VALUES ('username', 18),('username', 18),('username', 18),
+('username', 18),('username', 18),('username', 18),
+('username', 18),('username', 18),('username', 18),
+('username', 18),('username', 18),('username', 18),
+('username', 18),('username', 18),('username', 18),
+('username', 18),('username', 18),('username', 18),
+('username', 18),('username', 18);
+
+-- CREATE TABLE pledges (
+--   id INTEGER PRIMARY KEY,
+--   amount INTEGER,
+--   user_id INTEGER,
+--   project_id INTEGER,
+--   FOREIGN KEY(user_id) REFERENCES users(id),
+--   FOREIGN KEY (project_id) REFERENCES projects(id)
+--   );
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),
+(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),
+(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),
+(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),
+(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),
+(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17),(5000, 5, 17);
+
+
+
+
+
+
+
+
+
+
+
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,29 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+"SELECT title, SUM(amount) FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+"SELECT name, age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY name;"
 end
 
 def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT title, (funding_goal - SUM(amount)) AS difference FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY project_id HAVING difference <= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+"SELECT name, SUM(amount) as sum FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY name ORDER BY sum;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT category, amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+"SELECT category, SUM(amount) AS SUM
+ FROM projects 
+ JOIN pledges 
+ ON projects.id = pledges.project_id 
+ WHERE category = 'books'"
 end
